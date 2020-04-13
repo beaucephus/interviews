73c3b6d4d6cbf147b28e5d58174253c44387d744
Tyler Browning <josephtylerbrowning@gmail.com>
2020-04-13 15:23:22 -0400

    Add phreesia interview question.

---
+++ phreesia/fizzbuzz_spec.rb
@@ -0,0 +1,37 @@
+require_relative "fizzbuzz"
+
+RSpec.describe "fizzbuzz" do
+
+
+
+end
+
+RSpec.describe "max_validator" do
+
+
+  it "returns an Integer matching input" do
+    expect(max_validator(1)).to be(1)
+  end
+
+
+end
+
+RSpec.describe "fizz_validator" do
+
+  it "returns a String matching input" do
+    expect(fizz_validator('fizz')).to eq('fizz')
+  end
+
+  it "returns a String matching input" do
+    expect(fizz_validator('foo')).to eq('foo')
+  end
+
+  it "returns a default on null input" do
+    expect(fizz_validator(nil)).to eq('fizz')
+  end
+
+  it "returns a default on empty input" do
+    expect(fizz_validator("")).to eq('fizz')
+  end
+
+end
